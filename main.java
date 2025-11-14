95adac26982d314875fb92137b5cdb632550e07a
martin <martingayo900rgmail,com>
2025-11-14 01:20:18 +0100

    feat: Implementada la funcionalidad registro

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class Main { 
 public static void main(String[] args) { 
 System.out.println("Versió n inicial del proyecto."); 
+System.out.println("Funcionalidad de login implementada.");
 } 
 } 
